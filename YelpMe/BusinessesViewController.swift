62cfe09c46aee07f693a9defa23b6dfee7c538c9
Vnu <dearvnu@yahoo.in>
2016-02-13 13:15:08 -0800

    Better print module #3

--- YelpMe/BusinessesViewController.swift
+++ YelpMe/BusinessesViewController.swift
@@ -17,46 +17,29 @@ class BusinessesViewController: UIViewController {
     @IBOutlet var searchTableView: UITableView!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-
+       super.viewDidLoad()
        loadBusinesses()
-
-/* Example of Yelp search with more search options specified
-        Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
-            self.businesses = businesses
-            
-            for business in businesses {
-                print(business.name!)
-                print(business.address!)
-            }
-        }
-*/
     }
     func loadBusinesses(){
         YelpAPI.sharedInstance.searchWithTerm("Indian", completion: { (businesses: [Business]!, error: NSError!) -> Void in
             self.businesses = businesses
-//            for business in businesses {
-//                print(business.categories!)
-//            }
+            Business.printAll(businesses)
             self.searchTableView.reloadData()
         })
 
     }
+    
+    func searchBusinesses(){
+        YelpAPI.sharedInstance.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
+            self.businesses = businesses
+            Business.printAll(businesses)
+            self.searchTableView.reloadData()
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
